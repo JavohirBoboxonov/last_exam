93ab24043e4400e8000c76044039d69b3a3ad7b1
JavohirBoboxonov <javohirboboxonov08@gmail.com>
2026-05-18 21:43:22 +0500

    Stash changes before merge

--- vacancy/api.py
+++ vacancy/api.py
@@ -10,7 +10,6 @@ from vacancy.service import VacancyService
 
 router = APIRouter(prefix="/vacancies", tags=["Vacancies"])
 
-# 1. create_vacancy
 @router.post("/create", response_model=VacancyResponse, status_code=status.HTTP_201_CREATED)
 async def create_vacancy(
     vacancy_data: VacancyCreate,
@@ -18,8 +17,6 @@ async def create_vacancy(
     db: AsyncSession = Depends(get_db)
 ):  
     return await VacancyService.create_vacancy(db, vacancy_data, current_user)
-
-# 2. get_vacancies
 @router.get("/", response_model=VacancyListResponse)
 async def get_vacancies( # async qo'shildi
     skip: int = Query(0, ge=0),
@@ -39,10 +36,8 @@ async def get_vacancies( # async qo'shildi
         "salary_min_gte": salary_min_gte,
         "experience_required": experience_required
     }
-    # MUHIM: await qo'shildi
     return await VacancyService.get_all_vacancies(db, current_user, skip, limit, filters)
 
-# 3. get_my_vacancies
 @router.get("/my-vacancies", response_model=VacancyListResponse)
 async def get_my_vacancies(
     skip: int = Query(0, ge=0),
@@ -53,7 +48,6 @@ async def get_my_vacancies(
 ):
     return await VacancyService.get_my_vacancies(db, current_user, skip, limit, is_active)
 
-# 4. get_vacancy (ID bo'yicha)
 @router.get("/{vacancy_id}", response_model=VacancyResponse)
 async def get_vacancy(
     vacancy_id: int,
@@ -62,8 +56,7 @@ async def get_vacancy(
 ):
     return await VacancyService.get_vacancy_by_id(db, vacancy_id, current_user)
 
-# 5. update_vacancy
-@router.patch("/update{vacancy_id}", response_model=VacancyResponse)
+@router.patch("/update/{vacancy_id}", response_model=VacancyResponse)
 async def update_vacancy(
     vacancy_id: int,
     vacancy_data: VacancyUpdate,
